77a7d9cd135ecf4329340d61913b10bf5360b82a
ESME-TB <theo.blanc@esme.fr>
2020-09-11 17:06:10 +0200

    Removing old files

--- PC/ServClient_v2/trace_xgraph.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -f "./hello" ];
-then
-	./hello
-else
-	echo "*** Aucun fichier .xg disponible pour tracer ***";
-	echo "*** Lancez une Acquisition pour en créer un. ***";
-fi
--- new_PC/trace_xgraph.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -f "./hello" ];
-then
-#	launching trace with xgraph (./hello)
-	xgraph sinus.xg
-else
-	echo "*** Aucun fichier .xg disponible pour tracer ***";
-	echo "*** Lancez une Acquisition pour en créer un. ***";
-fi
